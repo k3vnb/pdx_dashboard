b19370a50c256595f8dc226bb9a8d8fc31131bc9
Kevin Boyle <papershack@gmail.com>
2019-05-04 18:58:51 -0700

    add loading spinner

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+const weatherKey = '';
+const igKey = '';
+const meetUpKey = '';
+
+function loadingSpinner(){
+    const overlay = document.getElementById("loading-spinner-overlay");
+
+    window.addEventListener('load', function(){
+        overlay.style.display = 'none';
+    });
+}
