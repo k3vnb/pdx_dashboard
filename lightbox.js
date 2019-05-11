9ec1b6ef96f1c6470310e36c0b22128ec3085c39
Kevin Boyle <papershack@gmail.com>
2019-05-10 19:26:23 -0700

    add functionality for displaying lightbox in DOM and removing it from DOM on click

--- lightbox.js
+++ lightbox.js
@@ -1,15 +1,30 @@
-function handleLightbox(){
+function handleCloseLightbox(){
+    $('.lightbox').on("click",() => {
+        console.log('yerp')
+        $('.lightbox').css('display', 'none')})
+}
+
+function displayLightbox(currentImage){
+    $('.lightbox').empty();
+    $('.lightbox').append(`
+        <img class="lightbox-photo" src=${currentImage.url} alt="Selected photo" />
+    `)
+    $('.lightbox').css('display', 'flex');
+    handleCloseLightbox();
+}
+
+function handleGalleryImageClick(){
     $('#picture-gallery').on("click", ".gallery-img", function(event){
         event.stopPropagation();
         let getID = this.id;
         const currentImage = STORE.imageGallery.find(img => img.id === Number(getID))
-        console.log(currentImage) 
+        displayLightbox(currentImage);
     });
 }
 
 
 $(
-    handleLightbox()
+    handleGalleryImageClick()
 );
 
 // const heroImg = $(event.currentTarget).find("img").attr("src");
